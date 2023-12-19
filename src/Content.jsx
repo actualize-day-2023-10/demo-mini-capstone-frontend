48464b52a57819fa97c2cee101f9f2cd6c1a4cad
brian <riceb53@gmail.com>
2023-12-19 09:20:52 -0800

    get data frmo rails, not on html page

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,17 @@
+import axios from 'axios'
+
 export function Content() {
+  const handleGetProducts = () => {
+    console.log('in get products');
+    axios.get("http://localhost:3000/products.json").then(response => {
+      console.log(response.data)
+    })
+  }
+  
   return (
     <div>
       <h1>Welcome to React!</h1>
+      <button onClick={handleGetProducts}>Get products</button>
     </div>
   )
 }
