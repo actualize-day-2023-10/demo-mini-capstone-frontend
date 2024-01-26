1404472e71faa1cc4b2aec36994b9c36f55013d8
brian <riceb53@gmail.com>
2024-01-26 12:03:53 -0800

    style header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,12 @@
 export function Header() {
   return (
     <header>
-      <nav>
-      <a href="/">Home</a> | <a href="/products/new">New Product</a> | <a href="/login">Login</a> | <a href="/signup">Signup</a> | <a href="/cart">Cart</a>
+      <nav className="bg-gray-800 text-gray-300 p-4">
+      <a className="rounded-xl p-2 hover:bg-gray-700 hover:text-white" href="/">Home</a>
+      <a className="rounded-xl p-2 hover:bg-gray-700 hover:text-white" href="/products/new">New Product</a>
+      <a className="rounded-xl p-2 hover:bg-gray-700 hover:text-white" href="/login">Login</a>
+      <a className="rounded-xl p-2 hover:bg-gray-700 hover:text-white" href="/signup">Signup</a>
+      <a className="rounded-xl p-2 hover:bg-gray-700 hover:text-white" href="/cart">Cart</a>
       </nav>
     </header>
   )
